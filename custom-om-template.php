31f5c73db474b815352c7a38adaf14ca83b1ae10
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-28 18:19:52 -0400

    Adds custom-om-template.php file to the project

---
+++ custom-om-template.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The Template for the Custom OM Builder page
+ *
+ * @package Betheme
+ * @author Sergio Cabral
+ * @link http://sergiocabral.io
+ */
+
+get_header();
+?>
+
+<!-- #Content -->
+<div id="Content">
+	<div class="content_wrapper clearfix">
+
+		<!-- .sections_group -->
+		<div class="sections_group">
+			
+		</div>
+			
+	</div>
+</div>
+
+<?php get_footer();
+
+// Omit Closing PHP Tags
