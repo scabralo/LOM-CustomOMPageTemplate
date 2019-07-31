010b63253dd6179617d5cc2839e4381a750ba3ef
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-31 14:40:49 -0400

    Updates to latest version of Custom OM Builder

--- custom-om-template.php
+++ custom-om-template.php
@@ -20,25 +20,71 @@ get_header();
       <div class="section">
         <div class="section_wrapper clearfix">
           <h1>Custom OM Builder</h1>
-          <div class="custom-om__left-column">
-            <div class="custom-om__om-type-container">
-              <h2>OM Type</h2>
+          <div class="custom-om__section-wrapper">
+            <div class="custom-om__left-column">
+              <div class="custom-om__om-type-container">
+                <h2>OM Type</h2>
+                <div class="custom-om__om-type-wrapper">
+                  <span class="custom-om__om-type-item">
+                    <h3>Plain</h3>
+                  </span>
+                  <span class="custom-om__om-type-item">
+                  <h3>Starholder</h3>
+                  </span>
+                </div>
+              </div>
+              <div class="custom-om__preview-container"></div>
             </div>
-            <div class="custom-om__preview-container"></div>
-          </div>
-          <div class="custom-om__right-column">
-            <h2>Customizations</h2>
-            <div class="custom-om__start-customizations">
-              <h3>Star</h3>
-            </div>
-            <div class="custom-om__hat-customizations">
-              <h3>Hat</h3>
-            </div>
-            <div class="custom-om__face-customizations">
-              <h3>Face</h3>
-            </div>
-            <div class="custom-om__body-customizations">
-              <h3>Body</h3>
+            <div class="custom-om__right-column">
+              <h2>Customizations</h2>
+              <div class="custom-om__start-customizations">
+                <h3>Star</h3>
+              </div>
+              <div class="custom-om__hat-customizations">
+                <h3>Hat</h3>
+                <div class="custom-om__hat-wrapper custom-om__custom-category">
+                  <span class="custom-om__hat-item" style="background-color: #E9E3CB"></span>
+                  <span class="custom-om__hat-item" style="background-color: #000000"></span>
+                  <span class="custom-om__hat-item" style="background-color: #494949"></span>
+                  <span class="custom-om__hat-item" style="background-color: #AFAFAF"></span>
+                  <span class="custom-om__hat-item" style="background-color: #C9C9C9"></span>
+                  <span class="custom-om__hat-item" style="background-color: #FFFFFF"></span>
+                  <span class="custom-om__hat-item" style="background-color: #6587C7"></span>
+                  <span class="custom-om__hat-item" style="background-color: #91B0E6"></span>
+                  <span class="custom-om__hat-item" style="background-color: #1A203A"></span>
+                  <span class="custom-om__hat-item" style="background-color: #243560"></span>
+                </div>
+              </div>
+              <div class="custom-om__face-customizations">
+                <h3>Face</h3>
+                <div class="custom-om__face-wrapper custom-om__custom-category">
+                  <span class="custom-om__face-item" style="background-color: #E9E3CB"></span>
+                  <span class="custom-om__face-item" style="background-color: #000000"></span>
+                  <span class="custom-om__face-item" style="background-color: #494949"></span>
+                  <span class="custom-om__face-item" style="background-color: #AFAFAF"></span>
+                  <span class="custom-om__face-item" style="background-color: #C9C9C9"></span>
+                  <span class="custom-om__face-item" style="background-color: #FFFFFF"></span>
+                  <span class="custom-om__face-item" style="background-color: #6587C7"></span>
+                  <span class="custom-om__face-item" style="background-color: #91B0E6"></span>
+                  <span class="custom-om__face-item" style="background-color: #1A203A"></span>
+                  <span class="custom-om__face-item" style="background-color: #243560"></span>
+                </div>
+              </div>
+              <div class="custom-om__body-customizations">
+                <h3>Body</h3>
+                <div class="custom-om__body-wrapper custom-om__custom-category">
+                  <span class="custom-om__body-item" style="background-color: #E9E3CB"></span>
+                  <span class="custom-om__body-item" style="background-color: #000000"></span>
+                  <span class="custom-om__body-item" style="background-color: #494949"></span>
+                  <span class="custom-om__body-item" style="background-color: #AFAFAF"></span>
+                  <span class="custom-om__body-item" style="background-color: #C9C9C9"></span>
+                  <span class="custom-om__body-item" style="background-color: #FFFFFF"></span>
+                  <span class="custom-om__body-item" style="background-color: #6587C7"></span>
+                  <span class="custom-om__body-item" style="background-color: #91B0E6"></span>
+                  <span class="custom-om__body-item" style="background-color: #1A203A"></span>
+                  <span class="custom-om__body-item" style="background-color: #243560"></span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
