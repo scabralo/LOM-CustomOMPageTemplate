f4eb987c3627dd3f66f1cdadbd9bc9554507cde0
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-30 22:12:22 -0400

    Adds minimum structure for Customizations Page

--- custom-om-template.php
+++ custom-om-template.php
@@ -20,6 +20,27 @@ get_header();
       <div class="section">
         <div class="section_wrapper clearfix">
           <h1>Custom OM Builder</h1>
+          <div class="custom-om__left-column">
+            <div class="custom-om__om-type-container">
+              <h2>OM Type</h2>
+            </div>
+            <div class="custom-om__preview-container"></div>
+          </div>
+          <div class="custom-om__right-column">
+            <h2>Customizations</h2>
+            <div class="custom-om__start-customizations">
+              <h3>Star</h3>
+            </div>
+            <div class="custom-om__hat-customizations">
+              <h3>Hat</h3>
+            </div>
+            <div class="custom-om__face-customizations">
+              <h3>Face</h3>
+            </div>
+            <div class="custom-om__body-customizations">
+              <h3>Body</h3>
+            </div>
+          </div>
         </div>
       </div>	
 		</div>
