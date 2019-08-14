f074ce651648f6814280f12b5c3e61e03238b6d3
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-14 13:45:22 -0400

    Adds colors array to on custom-om-template.js

--- custom-om-template.js
+++ custom-om-template.js
@@ -1,3 +1,36 @@
 ;(function() {
+  const colors = [
+    {
+      hexValue: '#E9E3CB'
+    },
+    {
+      hexValue: '#000000'
+    },
+    {
+      hexValue: '#494949'
+    },
+    {
+      hexValue: '#AFAFAF'
+    },
+    {
+      hexValue: '#C9C9C9'
+    },
+    {
+      hexValue: '#FFFFFF'
+    },
+    {
+      hexValue: '#6587C7'
+    },
+    {
+      hexValue: '#91B0E6'
+    },
+    {
+      hexValue: '#1A203A'
+    },
+    {
+      hexValue: '#243560'
+    },
+  ]
+
   
 })()
