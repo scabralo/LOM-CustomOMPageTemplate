bbab37b571710b2d65b4f2dcadca565ffd0c7019
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-19 18:33:16 -0400

    Adds customizationsSections, typeItem, and omTypesSection funcs to project

--- custom-om-template.js
+++ custom-om-template.js
@@ -139,6 +139,53 @@
     return node
   }
 
+  /*
+   * This function creates all the customizations sections of the page
+   * @param sections An array containing all the sections we're creating and their properties
+   * @param colors An array with all the colors available
+   * @return node In this case the node is a customizations section
+   */
+  const customizationsSections = (sections, colors) => {
+    const customizationsContainer = document.getElementById('custom-om__customizations-container')
+    const fragment = document.createDocumentFragment()
+
+    sections.map((section) => {
+      fragment.appendChild(customizationSection(section, colors))
+    })
+
+    customizationsContainer.appendChild(fragment)
+  }
+
+  /*
+   * This function returns an OM Type element 
+   * @param type An object containing all the properties for the type element we're creating
+   * @return node In this case the node is a span element
+   */
+  const typeItem = type => {
+    const node = document.createElement('span')
+    node.id = type.id
+    node.setAttribute('class', 'custom-om__om-type-item')
+
+    const heading = document.createElement('h3')
+    const text = document.createTextNode(type.title)
+    heading.appendChild(text)
+    node.appendChild(heading)
+
+    // Needs an image in the near future
+
+    return node
+  }
+
+  /*
+   * This function populates the OM TYpe section of the page
+   * @param types An object containing all the properties for the types we're creating
+   * @return node In this case the node is the OM Type section
+   */
+  const omTypesSection = (types) => {
+    const omTypesWrapper = document.getElementById('custom-om__om-type-wrapper')
+    const fragment = document.createDocumentFragment()
+  }
+
   /* App */
   
   const customizationsContainer = document.getElementById('custom-om__customizations-container')
